f59e136183eefdedf19dd66b307794ece6ccf08a
ashishsaini0194 <smartsainiashish@gmail.com>
2025-02-08 14:35:28 +0530

    replaced the url

--- src/components/PushNotification.js
+++ src/components/PushNotification.js
@@ -1,7 +1,7 @@
 export const showNotification = ({
   title = "Chat app",
   message = "New Notifcation",
-  imageUrl = "https://www.vkf-renzel.com/out/pictures/generated/product/1/356_356_75/r12044336-01/general-warning-sign-10836-1.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  imageUrl = "../../public/Images/warningSign.jpg",
 }) => {
   const options = {
     body: message,
